f0492f9a117e2c80b7092ad51a090c071a1793af
BaoAnh <baoanhtongthi@gmail.com>
2026-04-08 23:22:42 +0700

    Final: Move frontend to root and clean repository

--- teacher/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from './apiClient.js';
-
-export const dashboardService = {
-    async getTeacherStats() {
-        // khớp với api.php
-        const response = await apiClient.get('/teacher/course-classes');
-        return response.data;
-    }
-};
